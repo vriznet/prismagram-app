9185c8a653350649f1175f106ab9c6733eda2221
vriznet <vriz@vriz.net>
2020-07-28 11:14:23 +0900

    10.1 ~ 10.2 Preloading Assets & Cache

--- App.js
+++ App.js
@@ -1,19 +1,50 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from 'react';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+import { Asset } from 'expo-asset';
+import { AsyncStorage, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+import { Ionicons } from '@expo/vector-icons';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { persistCache } from 'apollo-cache-persist';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo-hooks';
+import options from './apollo';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Expo First App!</Text>
-    </View>
+  const [loaded, setLoaded] = useState(false);
+  const [client, setClient] = useState(null);
+  const preLoad = async () => {
+    try {
+      console.log(loaded);
+      await Font.loadAsync(Ionicons.font);
+      await Asset.loadAsync([require('./assets/icon.png')]);
+      const cache = new InMemoryCache();
+      await persistCache({
+        cache,
+        storage: AsyncStorage,
+      });
+      const client = new ApolloClient({
+        cache,
+        ...options,
+      });
+      setLoaded(true);
+      setClient(client);
+      console.log(loaded);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    preLoad();
+  }, []);
+  return loaded && client ? (
+    <ApolloProvider client={client}>
+      <View>
+        <Text>Expo First App!</Text>
+      </View>
+    </ApolloProvider>
+  ) : (
+    <AppLoading />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
