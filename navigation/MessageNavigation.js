4bce83dc9fb1999d5a62eddaf482b09dbc3a3a54
vriznet <vriz@vriz.net>
2020-08-26 15:35:05 +0900

    11.4 Message Navigation part One

---
+++ navigation/MessageNavigation.js
@@ -0,0 +1,5 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import Messages from '../screens/Message/Messages';
+import Message from '../screens/Message/Message';
+
+export default createStackNavigator({});
--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,19 +1,44 @@
+import React from 'react';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
-import { View } from 'react-native';
-import Home from '../screens/Home';
-import Search from '../screens/Search';
-import Notifications from '../screens/Notifications';
-import Profile from '../screens/Profile';
+import { createStackNavigator } from 'react-navigation-stack';
+import { View, Text, TouchableOpacity } from 'react-native';
+import Home from '../screens/Tab/Home';
+import Search from '../screens/Tab/Search';
+import Notifications from '../screens/Tab/Notifications';
+import Profile from '../screens/Tab/Profile';
+
+const stackFactory = (initialRoute, customConfig) =>
+  createStackNavigator({
+    InitialRoute: {
+      screen: initialRoute,
+      navigationOptions: { ...customConfig },
+    },
+  });
 
 export default createBottomTabNavigator({
-  Home,
-  Search,
+  Home: {
+    screen: stackFactory(Home, {
+      title: 'Home',
+      headerRight: () => (
+        <TouchableOpacity>
+          <Text>Hello</Text>
+        </TouchableOpacity>
+      ),
+    }),
+  },
+  Search: {
+    screen: stackFactory(Search, { title: 'Search' }),
+  },
   Add: {
     screen: View,
     navigationOptions: {
       tabBarOnPress: ({ navigation }) => navigation.navigate('PhotoNavigation'),
     },
   },
-  Notifications,
-  Profile,
+  Notifications: {
+    screen: stackFactory(Notifications, { title: 'Notifications' }),
+  },
+  Profile: {
+    screen: stackFactory(Profile, { title: 'Profile' }),
+  },
 });
