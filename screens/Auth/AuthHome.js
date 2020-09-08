5f0b2f29ab9a6dee9f6a0bece3e1d69b7f6f9892
vriznet <vriz@vriz.net>
2020-09-08 18:24:33 +0900

    12.0 AuthHome

--- screens/Auth/AuthHome.js
+++ screens/Auth/AuthHome.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import constants from '../../constants';
 
 const View = styled.View`
   justify-content: center;
@@ -8,16 +9,43 @@ const View = styled.View`
   flex: 1;
 `;
 
-const Text = styled.Text``;
+const Image = styled.Image`
+  width: ${constants.width / 3}px;
+  margin-bottom: 10px;
+`;
+
+const Touchable = styled.TouchableOpacity``;
+
+const SignUpBtn = styled.View`
+  background-color: ${(props) => props.theme.blueColor};
+  padding: 10px 0px;
+  border-radius: 4px;
+  width: ${constants.width / 2}px;
+  margin-bottom: 20px;
+`;
+const SignUpBtnText = styled.Text`
+  color: white;
+  text-align: center;
+  font-weight: 600;
+`;
+
+const LoginLink = styled.View``;
+const LoginLinkText = styled.Text`
+  color: ${(props) => props.theme.blueColor};
+`;
 
 export default ({ navigation }) => (
   <View>
-    <Text>Auth Home</Text>
-    <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-      <Text>Go to Login</Text>
-    </TouchableOpacity>
-    <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
-      <Text>Go to Signup</Text>
-    </TouchableOpacity>
+    <Image resizeMode={'contain'} source={require('../../assets/logo.png')} />
+    <Touchable onPress={() => navigation.navigate('SignUp')}>
+      <SignUpBtn>
+        <SignUpBtnText>Create New Account</SignUpBtnText>
+      </SignUpBtn>
+    </Touchable>
+    <Touchable onPress={() => navigation.navigate('Login')}>
+      <LoginLink>
+        <LoginLinkText>Login</LoginLinkText>
+      </LoginLink>
+    </Touchable>
   </View>
 );
