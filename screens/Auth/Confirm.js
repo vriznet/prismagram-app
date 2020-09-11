9f53e714e45e421adb066041d4fae87501d9ccee
vriznet <vriz@vriz.net>
2020-09-11 22:55:31 +0900

    12.6 Confirm

--- screens/Auth/Confirm.js
+++ screens/Auth/Confirm.js
@@ -1,16 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { TouchableWithoutFeedback, Keyboard } from 'react-native';
+import AuthButton from '../../components/AuthButton';
+import AuthInput from '../../components/AuthInput';
+import useInput from '../../hooks/useInput';
+import { Alert } from 'react-native';
+import { useMutation } from 'react-apollo-hooks';
+import { CONFIRM_SECRET } from './AuthQueries';
+import { useLogUserIn } from '../../AuthContext';
 
 const View = styled.View`
   justify-content: center;
   align-items: center;
   flex: 1;
+  background-color: white;
 `;
 
-const Text = styled.Text``;
-
-export default () => (
-  <View>
-    <Text>Confirm</Text>
-  </View>
-);
+export default ({ navigation }) => {
+  const confirmInput = useInput('');
+  const logIn = useLogUserIn();
+  const [loading, setLoading] = useState(false);
+  const [confirmSecretMutation] = useMutation(CONFIRM_SECRET, {
+    variables: {
+      secret: confirmInput.value,
+      email: navigation.getParam('email'),
+    },
+  });
+  const handleConfirm = async () => {
+    const { value } = confirmInput;
+    if (value === '' || !value.includes(' ')) {
+      return Alert.alert('Invalid secret');
+    }
+    try {
+      setLoading(true);
+      const {
+        data: { confirmSecret },
+      } = await confirmSecretMutation();
+      if (confirmSecret !== '' || confirmSecret !== false) {
+        logIn(confirmSecret);
+      } else {
+        Alert.alert('Wrong secret!');
+      }
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Cannot confirm secret.');
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View>
+        <AuthInput
+          {...confirmInput}
+          placeholder="Secret"
+          autoCapitalize="none"
+          returnKeyType="send"
+          onSubmitEditing={handleConfirm}
+          autoCorrect={false}
+        />
+        <AuthButton loading={loading} text="Confirm" onPress={handleConfirm} />
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
--- screens/Auth/Login.js
+++ screens/Auth/Login.js
@@ -40,11 +40,11 @@ export default ({ navigation }) => {
       } = await requestSecretMutation();
       if (requestSecret) {
         Alert.alert('Check your email');
-        navigation.navigate('Confirm');
+        navigation.navigate('Confirm', { email: value });
         return;
       } else {
         Alert.alert('Account not found');
-        navigation.navigate('SignUp');
+        navigation.navigate('SignUp', { email: value });
       }
     } catch (error) {
       console.log(error);
