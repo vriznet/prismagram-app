ec460767c84fc2126877cbbd959dfaeb40e2f442
vriznet <vriz@vriz.net>
2020-10-22 19:44:28 +0900

    18.0 Tab Styles

--- navigation/PhotoNavigation.js
+++ navigation/PhotoNavigation.js
@@ -3,25 +3,56 @@ import { createMaterialTopTabNavigator } from 'react-navigation-tabs';
 import SelectPhoto from '../screens/Photo/SelectPhoto';
 import TakePhoto from '../screens/Photo/TakePhoto';
 import Upload from '../screens/Photo/Upload';
+import style from '../style';
 import { stackStyles } from './config';
 
 const PhotoTabs = createMaterialTopTabNavigator(
   {
-    SelectPhoto,
-    TakePhoto,
+    Select: {
+      screen: SelectPhoto,
+      navigationOptions: {
+        tabBarLabel: 'Select',
+      },
+    },
+    Take: {
+      screen: TakePhoto,
+      navigationOptions: {
+        tabBarLabel: 'Take',
+      },
+    },
   },
   {
     tabBarPosition: 'bottom',
+    tabBarOptions: {
+      indicatorStyle: {
+        backgroundColor: style.blackColor,
+        marginBottom: 65,
+      },
+      labelStyle: {
+        color: style.blackColor,
+        fontWeight: '600',
+      },
+      style: {
+        paddingBottom: 20,
+        ...stackStyles,
+      },
+    },
   }
 );
 
 export default createStackNavigator(
   {
-    PhotoTabs,
+    Tabs: {
+      screen: PhotoTabs,
+      navigationOptions: {
+        headerShown: false,
+      },
+    },
     Upload,
   },
   {
     defaultNavigationOptions: {
+      title: 'Photo',
       headerStyle: {
         ...stackStyles,
       },
