3bba3725dc4627249151f231db9609d29e243441
vriznet <vriz@vriz.net>
2020-10-22 09:47:30 +0900

    17.0 Profile Screen

--- screens/Tab/Profile.js
+++ screens/Tab/Profile.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
+import { gql } from 'apollo-boost';
+import { USER_FRAGMENT } from '../../fragments';
+import { ScrollView } from 'react-native';
+import { useQuery } from 'react-apollo-hooks';
+import Loader from '../../components/Loader';
+import UserProfile from '../../components/UserProfile';
 
-const View = styled.View`
-  justify-content: center;
-  align-items: center;
-  flex: 1;
+const ME = gql`
+  {
+    me {
+      ...UserParts
+    }
+  }
+  ${USER_FRAGMENT}
 `;
 
 const Text = styled.Text``;
 
-export default () => (
-  <View>
-    <Text>Profile</Text>
-  </View>
-);
+export default ({ navigation }) => {
+  const { loading, data } = useQuery(ME);
+
+  return (
+    <ScrollView>
+      {loading ? <Loader /> : data && data.me && <UserProfile {...data.me} />}
+    </ScrollView>
+  );
+};
