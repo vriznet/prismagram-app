944a5a9edd6bc901812e87b018e45487434812f1
vriznet <vriz@vriz.net>
2020-10-23 16:21:38 +0900

    18.5 Saving Photo

--- screens/Photo/TakePhoto.js
+++ screens/Photo/TakePhoto.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Platform, TouchableOpacity } from 'react-native';
 import * as Permissions from 'expo-permissions';
+import * as MediaLibrary from 'expo-media-library';
 import { Camera } from 'expo-camera';
 import styled from 'styled-components';
 import Loader from '../../components/Loader';
@@ -10,12 +11,41 @@ import style from '../../style';
 
 const View = styled.View`
   flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Icon = styled.View``;
+
+const Button = styled.View`
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+  border: 20px solid ${style.lightGrayColor};
 `;
 
 export default () => {
+  const cameraRef = useRef();
+  const [canTakePhoto, setCanTakePhoto] = useState(true);
   const [loading, setLoading] = useState(true);
   const [hasPermission, setHasPermission] = useState(false);
   const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+
+  const takePhoto = async () => {
+    if (!canTakePhoto) {
+      return;
+    }
+    try {
+      setCanTakePhoto(false);
+      const { uri } = await cameraRef.current.takePictureAsync({
+        quality: 1,
+      });
+      const asset = await MediaLibrary.createAssetAsync(uri);
+    } catch (e) {
+      console.log(e);
+      setCanTakePhoto(true);
+    }
+  };
 
   const askPermission = async () => {
     try {
@@ -47,27 +77,37 @@ export default () => {
       {loading ? (
         <Loader />
       ) : hasPermission ? (
-        <Camera
-          type={cameraType}
-          style={{
-            width: constants.width,
-            height: constants.height / 2,
-            justifyContent: 'flex-end',
-            padding: 10,
-          }}
-        >
-          <TouchableOpacity onPress={toggleType}>
-            <Ionicons
-              name={
-                Platform.OS === 'ios'
-                  ? 'ios-reverse-camera'
-                  : 'md-reverse-camera'
-              }
-              size={28}
-              color={style.blackColor}
-            />
-          </TouchableOpacity>
-        </Camera>
+        <React.Fragment>
+          <Camera
+            ref={cameraRef}
+            type={cameraType}
+            style={{
+              width: constants.width,
+              height: constants.height / 2,
+              justifyContent: 'flex-end',
+              padding: 10,
+            }}
+          >
+            <TouchableOpacity onPress={toggleType}>
+              <Icon>
+                <Ionicons
+                  name={
+                    Platform.OS === 'ios'
+                      ? 'ios-reverse-camera'
+                      : 'md-reverse-camera'
+                  }
+                  size={28}
+                  color={'white'}
+                />
+              </Icon>
+            </TouchableOpacity>
+          </Camera>
+          <View>
+            <TouchableOpacity onPress={takePhoto} disabled={!canTakePhoto}>
+              <Button />
+            </TouchableOpacity>
+          </View>
+        </React.Fragment>
       ) : null}
     </View>
   );
