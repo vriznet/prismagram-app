561bd1437a666a7a6da27985ad2067f266bb56fa
vriznet <vriz@vriz.net>
2020-10-23 22:55:32 +0900

    18.7 Preparing from Upload

--- screens/Photo/Upload.js
+++ screens/Photo/Upload.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Image,
+  TouchableOpacity,
+  ActivityIndicator,
+  Alert,
+} from 'react-native';
 import styled from 'styled-components';
+import useInput from '../../hooks/useInput';
+import style from '../../style';
 
 const View = styled.View`
   justify-content: center;
@@ -7,10 +15,81 @@ const View = styled.View`
   flex: 1;
 `;
 
-const Text = styled.Text``;
+const Container = styled.View`
+  position: relative;
+  top: -50px;
+`;
 
-export default ({ navigation }) => (
-  <View>
-    <Text>I should upload {navigation.getParam('photo').uri}</Text>
-  </View>
-);
+const Form = styled.View``;
+
+const STextInput = styled.TextInput`
+  width: 300px;
+  height: 36px;
+  background-color: ${style.lightGrayColor};
+  margin-bottom: 10px;
+  padding-left: 14px;
+  border-radius: 8px;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  align-items: center;
+`;
+
+const Button = styled.View`
+  width: 300px;
+  height: 36px;
+  background-color: ${style.blueColor};
+  border-radius: 8px;
+  align-items: center;
+  justify-content: center;
+  margin-top: 12px;
+`;
+
+const Text = styled.Text`
+  color: white;
+  font-weight: 700;
+`;
+
+export default ({ navigation }) => {
+  const [loading, setIsLoading] = useState(false);
+  const [fileUrl, setFileUrl] = useState('');
+  const captionInput = useInput('');
+  const locationInput = useInput('');
+  const handleSubmit = async () => {
+    if (captionInput.value === '' || locationInput.value === '') {
+      Alert.alert('All fields are required');
+    }
+  };
+  return (
+    <View>
+      <Container>
+        <Image
+          source={{ uri: navigation.getParam('photo').uri }}
+          style={{ height: 300, width: 300, marginBottom: 20 }}
+        />
+        <Form>
+          <STextInput
+            onChangeText={captionInput.onChange}
+            value={captionInput.value}
+            placeholder="Caption"
+            placeholderTextColor={style.darkGrayColor}
+          />
+          <STextInput
+            onChangeText={locationInput.onChange}
+            value={locationInput.value}
+            placeholder="Location"
+            placeholderTextColor={style.darkGrayColor}
+          />
+          <TouchableOpacity onPress={handleSubmit}>
+            <Button>
+              {loading ? (
+                <ActivityIndicator color="white" />
+              ) : (
+                <Text>Upload</Text>
+              )}
+            </Button>
+          </TouchableOpacity>
+        </Form>
+      </Container>
+    </View>
+  );
+};
